b6930301e04221ee36bac3b37d3735b7c4f7d6d8
DESKTOP-TAPJHRS\Linus Dahlblom <46130@gymnasium.ax>
2022-12-26 11:37:41 +0200

    Skrev klart login.html och login.js

--- public_html/tabellovning/js/login.js
+++ public_html/tabellovning/js/login.js
@@ -1,5 +1,9 @@
-queryString = window.location.search;
+window.onload = function() {
+    // Hämta användartyp ur URL-parametrar
+    queryString = window.location.search;
     urlParams = new URLSearchParams(queryString);
-
     let userType = urlParams.get('userType');
+
+    // Lägg till användartyp i html-formulär
     document.getElementById("userType").value=userType;
+}
