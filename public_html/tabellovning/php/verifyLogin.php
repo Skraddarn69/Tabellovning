473e56e438d8b098daf2e71b20df5f3a3b8e9b45
DESKTOP-TAPJHRS\Linus Dahlblom <46130@gymnasium.ax>
2022-12-27 13:22:21 +0200

    Uppdaterade verifyLogin.php

--- public_html/tabellovning/php/verifyLogin.php
+++ public_html/tabellovning/php/verifyLogin.php
@@ -2,7 +2,59 @@
 declare (strict_types=1);
 require_once 'functions.php';
 
-$username = $_POST['username'];
-$password = $_POST['password'];
-$userType = $_POST['usertype'];
+# Tar emot POST-input
+$username = filter_input(INPUT_POST, 'username', FILTER_UNSAFE_RAW);
+$username = strip_tags($username);
+$password = filter_input(INPUT_POST, 'password', FILTER_UNSAFE_RAW);
+$password = strip_tags($password);
+$userType = filter_input(INPUT_POST, 'userType', FILTER_SANITIZE_NUMBER_INT);
 
+# Checkar att indatan är giltig
+if($username==="") {
+    $error = new stdClass();
+    $error -> error = ["Felaktig indata", "'username' får endast bestå av bokstäver och inte vara tomt"];
+    skickaSvar($error, 400);
+}
+
+if($password==="") {
+    $error = new stdClass();
+    $error -> error = ["Felaktig indata", "'password' får inte vara tomt"];
+    skickaSvar($error, 400);
+}
+
+if($userType!=0 && $userType!=1 || $userType==="") {
+    $error = new stdClass();
+    $error -> error = ["Felaktig indata", "'userType' får endast vara 0 eller 1"];
+    skickaSvar($error, 400);
+}
+
+if($userType===0) {
+    $sql = "SELECT losenord FROM larare WHERE anvandarnamn=:username";
+} else {
+    $sql = "SELECT losenord FROM elever WHERE anvandarnamn=:username";
+}
+
+$db = kopplaDatabas();
+
+$stmt = $db -> prepare($sql);
+$stmt -> execute(['username'=>$username]);
+
+if(!$stmt -> execute()) {
+    $error = new stdClass();
+    $error -> error = ["Fel vid databasanrop", $db->errorInfo()];
+    skickaSvar($error, 400);
+}
+
+if($record = $stmt->fetchObject()) {
+    $out = new stdClass();
+    if(password_verify($password, $record -> losenord)) {
+        $out -> response = True;
+    } else {
+        $out -> response = False;
+    }
+    skickaSvar($out, 200);
+} else {
+    $out = new stdClass();
+    $out -> error = ["Post saknas", "användaren:$username finns inte"];
+    skickaSvar($out, 400);
+}
