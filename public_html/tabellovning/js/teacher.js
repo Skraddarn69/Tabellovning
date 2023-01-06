20308d8bd17279cf41951101c3412341ed7e171f
DESKTOP-TAPJHRS\Linus Dahlblom <46130@gymnasium.ax>
2023-01-06 19:09:35 +0200

    Uppdaterade teacher.js

--- public_html/tabellovning/js/teacher.js
+++ public_html/tabellovning/js/teacher.js
@@ -14,8 +14,11 @@ function getClasses(ID) {
             }
         })
         .then(function(data) {
-            // Fyll dropdown med klasser
-            appendClasses(data);
+            if(data.noClasses) {
+                console.log("Inga klasser")
+            } else {
+                appendClasses(data);
+            }
         })
     }
 }
@@ -30,9 +33,12 @@ function appendClasses(data) {
         option = document.createElement("option");
         option.innerHTML = thisClass.klass;
         option.value = thisClass.ID;
-        option.onchange = getStudents(thisClass.ID)
         dropdown.appendChild(option);
+        dropdown.onchange = function() {
+            getStudents(dropdown.value);
+        }
     }
+    getStudents(data.classes[0].ID);
 }
 
 function getStudents(ID) {
@@ -44,13 +50,54 @@ function getStudents(ID) {
         }
     })
     .then(function(data) {
-        // Fyll tabell med elever
-        appendStudents(data);
+        // Rensa tabell
+        let table = document.getElementById("studentList");
+        while(table.childNodes[1]) {
+            table.removeChild(table.childNodes[1]);
+        }
+        
+        if(data.noStudents) {
+            console.log("Inga elever")
+        } else {
+            appendStudents(data);
+        }
     })
 }
 
 function appendStudents(data) {
+    let student;
+    let row;
+    let ID;
+    let firstname;
+    let lastname;
+    let username;
     let table = document.getElementById("studentList");
 
-    
+    // Lägg till tabellrader för alla elever
+    for(let i = 0; i < data.students.length; i++) {
+        student = data.students[i];
+        row = document.createElement("tr");
+        row.onclick = function() {
+            row.style.background_color = "blue";
+        }
+
+        ID = document.createElement("td");
+        ID.innerHTML = student.ID;
+        ID.style.display = "none";
+
+        firstname = document.createElement("td");
+        firstname.innerHTML = student.fornamn;
+
+        lastname = document.createElement("td");
+        lastname.innerHTML = student.efternamn;
+
+        username = document.createElement("td");
+        username.innerHTML = student.anvandarnamn;
+
+        row.appendChild(ID);
+        row.appendChild(firstname);
+        row.appendChild(lastname);
+        row.appendChild(username);
+        table.appendChild(row);
+    }
 }
